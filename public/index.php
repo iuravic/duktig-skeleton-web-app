b1b0d524093c63e321924ce5b6fb6fd0d8bf7bf2
Ivan Uravic <iuravic@gmail.com>
2017-06-05 17:41:52 +0200

    corrected path to autoloader

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/../../../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = (new \Duktig\Core\AppFactory)->make(
     __DIR__.'/../src/Config/config.php',
